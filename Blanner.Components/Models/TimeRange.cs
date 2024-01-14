5703a7aa83ddd823fe1930257d7aa6ca16b4033a
Tarxos <ftarxos@gmail.com>
2024-01-14 17:56:12 +0300

    replace time range component to library project

--- Blanner.Components/Models/TimeRange.cs
+++ Blanner.Components/Models/TimeRange.cs
@@ -1,5 +1,4 @@
-﻿namespace Blanner.Client.Models;
-
+﻿namespace Blanner.Components.Models;
 public class TimeRange {
     public DateTimeOffset Start { get; set; }
     public DateTimeOffset End { get; set; }
