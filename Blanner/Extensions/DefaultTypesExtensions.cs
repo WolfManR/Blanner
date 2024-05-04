7b2a3c19000a78bef536fe639cffda0b591f1c70
Tarxos <ftarxos@gmail.com>
2024-05-05 00:56:57 +0300

    rework time range edit dialog using FluentUI

--- Blanner/Extensions/DefaultTypesExtensions.cs
+++ Blanner/Extensions/DefaultTypesExtensions.cs
@@ -9,5 +9,47 @@ public static class DefaultTypesExtensions {
         {
             changeBehavior.Invoke(item);
         }
-    }
+	}
+
+	/// <summary>
+	/// Converts the DateOnly to an equivalent DateTime.
+	/// </summary>
+	/// <param name="value"></param>
+	/// <returns></returns>
+	public static DateTime ToDateTime(this DateOnly value) {
+		return value.ToDateTime(TimeOnly.MinValue);
+	}
+
+	/// <summary>
+	/// Converts the nullable DateTime to an equivalent DateOnly, removing the time part.
+	/// Returns <see cref="DateOnly.MinValue"/> if the <paramref name="value"/> is null.
+	/// </summary>
+	/// <param name="value"></param>
+	/// <returns></returns>
+	public static DateOnly ToDateOnly(this DateTime? value) {
+		return value == null ? DateOnly.MinValue : DateOnly.FromDateTime(value.Value);
+	}
+
+	/// <summary>
+	/// Converts the TimeOnly to an equivalent DateTime.
+	/// </summary>
+	/// <param name="value"></param>
+	/// <returns></returns>
+	public static DateTime ToDateTime(this TimeOnly value) {
+		return new DateTime(value.Ticks);
+	}
+
+	/// <summary>
+	/// Converts the nullable DateTime to an equivalent TimeOnly, removing the time part.
+	/// Returns <see cref="TimeOnly.MinValue"/> if the <paramref name="value"/> is null.
+	/// </summary>
+	/// <param name="value"></param>
+	/// <returns></returns>
+	public static TimeOnly ToTimeOnly(this DateTime? value) {
+		return value == null ? TimeOnly.MinValue : TimeOnly.FromDateTime(value.Value);
+	}
+
+	public static DateTime Normalize(this DateTime value) {
+		return value.Date + new TimeSpan(value.Hour, value.Minute, 0);
+	}
 }
---
+++ Blanner/Models/TimeRangeDialogParameters.cs
@@ -0,0 +1,11 @@
+﻿using Blanner.Components.Shared.Models;
+
+using Microsoft.AspNetCore.Components;
+
+namespace Blanner.Models;
+
+public class TimeRangeDialogParameters {
+	public TimeRange? TimeRange { get; set; }
+    public bool StartEditable { get; set; } = true;
+    public bool EndEditable { get; set; } = true;
+}
